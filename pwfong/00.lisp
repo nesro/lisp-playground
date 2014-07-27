7d9428f918ccd9411e0bf21fc9408732c0468437
Tomas Nesrovnal <nesro@nesro.cz>
2014-07-27 23:29:29 +0200

    ohai edit

--- pwfong/00.lisp
+++ pwfong/00.lisp
@@ -2,7 +2,11 @@
 |#
 
 (defun ohai ()
-  (format t "Oh hi!~%")
-  (quit))
+	(format t "Oh hi!~%")
+	(format t "Another line.~%")
+	(quit)
+)
 
+(ohai)
+; not reachable, because of the (quit) in the ohai function
 (ohai)
